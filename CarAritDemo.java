156daf90e44b47a1c18eef7ba26ce9be96c4b177
DiegoHernandez <diego0930@gmail.com>
2024-08-14 07:54:03 -0500

    Practica Java

---
+++ CarAritDemo.java
@@ -0,0 +1,12 @@
+// Las variables de carácter se manejan como enteros.
+class CarAritDemo {
+    public static void main(String args[]) {
+    char ch;
+    ch = 'X';
+    System.out.println("ch contiene " + ch);
+   //ch++; // incrementa ch Es posible incrementar char
+   // System.out.println("ch es ahora " + ch);
+    //ch = 90; // da a ch el valor Z A char puede asignársele un valor entero.
+    //System.out.println("ch es ahora " + ch);
+    }
+    }
---
+++ Ejemplo.java
@@ -0,0 +1,10 @@
+/*
+Éste es un programa simple de Java.
+Llame a este archivo Ejemplo.java.
+*/
+class Ejemplo {
+// Un programa de Java empieza con una llamada a main().
+public static void main(String args[]) {
+System.out.println("Java dirige Web.");
+}
+}
